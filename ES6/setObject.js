9c26bf897b69566686700dbea24389449f2fee95
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-24 11:19:35 +0530

    learn and practice Set objects

---
+++ ES6/setObject.js
@@ -0,0 +1,13 @@
+let newSet = new Set();
+newSet.add(1);
+newSet.add(1);
+newSet.add(2);
+newSet.add(2);
+newSet.add(3);
+newSet.add(4);
+newSet.add(4);
+newSet.add(4);
+newSet.add(5);
+newSet.add(6);
+
+console.log(newSet); //Set(6) {1, 2, 3, 4, 5, 6}
