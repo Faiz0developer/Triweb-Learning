f30bd6acf4f9624a90157c8287264f008d591210
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-24 11:30:07 +0530

    learn and practice class

---
+++ ES6/class.js
@@ -0,0 +1,12 @@
+class Student {
+  constructor(sName, classId) {
+    this.sName = sName;
+    this.classId = classId;
+  }
+}
+
+const std1 = new Student("Faizan", 6);
+const std2 = new Student("Aman", "7th");
+
+console.log(std1);
+console.log(std2);
