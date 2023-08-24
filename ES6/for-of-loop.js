6a2f55a786085bbd2f9731dba95ecd5d0d05b082
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-24 11:09:08 +0530

    practice for-of loop

---
+++ ES6/for-of-loop.js
@@ -0,0 +1,13 @@
+// for of loop in array
+let arr = [1, 2, 3, 4];
+
+for (num of arr) {
+  console.log(num);
+}
+
+// for of loop in stirng
+
+let str = "Faizan";
+for (let strs of str) {
+  console.log(strs);
+}
