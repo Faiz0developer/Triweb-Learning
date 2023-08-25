e1998b102204922c1ea4cd680406ec671a38caaa
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-25 13:31:03 +0530

    Learn and implement router and architecture like controller

--- NodeJS/get-post-express/app.js
+++ NodeJS/get-post-express/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const calculatorRouter = require("../get-post-express/routers/calculator");
 
 // create app or server
 const app = express();
@@ -13,47 +14,49 @@ app.get("/", (req, res) => {
   res.send("I am response");
 });
 
-app.get("/add", (req, res) => {
-  let n1 = 4;
-  let n2 = 3;
-  let n3 = n1 + n2;
-  //   res.send(`Sum is ${n3}`)   // RangeError
-
-  res.status(200).send(`Sum is ${n3.toString()}`);
-  //   res.send("Addition");
-});
-
-// sending response using POST method
-app.post("/add", (req, res) => {
-  //   console.log(req.body.num1);  //34
-  let n1 = req.body.num1;
-  let n2 = req.body.num2;
-  let sum = n1 + n2;
-  res.send(`Sum = ${sum.toString()}`);
-});
-
-app.post("/subtract", (req, res) => {
-  //   console.log(req.body.num1);  //34
-  let n1 = req.body.num1;
-  let n2 = req.body.num2;
-  let subtract = n1 - n2;
-  res.send(`Subtract = ${subtract.toString()}`);
-});
-
-app.post("/multiply", (req, res) => {
-  //   console.log(req.body.num1);  //34
-  let n1 = req.body.num1;
-  let n2 = req.body.num2;
-  let multiply = n1 * n2;
-  res.send(`Multiply = ${multiply.toString()}`);
-});
-
-app.post("/division", (req, res) => {
-  //   console.log(req.body.num1);  //34
-  let n1 = req.body.num1;
-  let n2 = req.body.num2;
-  let result = n1 / n2;
-  res.send(`Quotient = ${result.toString()}`);
-});
+app.use("/calculator", calculatorRouter);
 
 app.listen(3000);
+
+// app.get("/add", (req, res) => {
+//   let n1 = 4;
+//   let n2 = 3;
+//   let n3 = n1 + n2;
+//   //   res.send(`Sum is ${n3}`)   // RangeError
+
+//   res.status(200).send(`Sum is ${n3.toString()}`);
+//   //   res.send("Addition");
+// });
+
+// sending response using POST method
+// app.post("/add", (req, res) => {
+//   //   console.log(req.body.num1);  //34
+//   let n1 = req.body.num1;
+//   let n2 = req.body.num2;
+//   let sum = n1 + n2;
+//   res.send(`Sum = ${sum.toString()}`);
+// });
+
+// app.post("/subtract", (req, res) => {
+//   //   console.log(req.body.num1);  //34
+//   let n1 = req.body.num1;
+//   let n2 = req.body.num2;
+//   let subtract = n1 - n2;
+//   res.send(`Subtract = ${subtract.toString()}`);
+// });
+
+// app.post("/multiply", (req, res) => {
+//   //   console.log(req.body.num1);  //34
+//   let n1 = req.body.num1;
+//   let n2 = req.body.num2;
+//   let multiply = n1 * n2;
+//   res.send(`Multiply = ${multiply.toString()}`);
+// });
+
+// app.post("/division", (req, res) => {
+//   //   console.log(req.body.num1);  //34
+//   let n1 = req.body.num1;
+//   let n2 = req.body.num2;
+//   let result = n1 / n2;
+//   res.send(`Quotient = ${result.toString()}`);
+// });
