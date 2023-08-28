2e7743efad9679c8ae91e84d129cdd3a65362ab8
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-28 18:07:12 +0530

    implementation of if-else statement

---
+++ NodeJS/condition.js
@@ -0,0 +1,13 @@
+let age = 20;
+
+if (age > 18) {
+  console.log("You can vote");
+} else console.log("You can not vote");
+
+const a = 20;
+const b = 30;
+const c = 40;
+
+if (a > b && a > c) console.log("a is greatest");
+else if (b > c) console.log("b is greatest");
+else console.log("c is greatest");
