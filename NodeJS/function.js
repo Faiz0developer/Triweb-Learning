a9c03c13289c2d3f0bfff652a136585779ecdc52
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-29 00:14:50 +0530

    implement functions with different functionality like addition and subtraction

---
+++ NodeJS/function.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  var sum = num1 + num2;
+  console.log(sum);
+}
+add(25, 30);
+
+function subract(num1, num2) {
+  var subtract = num1 - num2;
+  console.log(subtract);
+}
+subract(40, 30);
+
+function multiply(num1, num2) {
+  var product = num1 * num2;
+  console.log(product);
+}
+multiply(5, 3);
+
+function divide(num1, num2) {
+  var quotient = num1 / num2;
+  console.log(quotient);
+}
+divide(30, 3);
