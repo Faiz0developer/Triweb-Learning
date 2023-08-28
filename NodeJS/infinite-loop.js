add76c5a4974f18c8b5c71534fdafefbbfa7b28c
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-28 23:45:26 +0530

    practice infinite loop

---
+++ NodeJS/infinite-loop.js
@@ -0,0 +1,13 @@
+// let num = 1;
+// while (true) {
+//   console.log("Press 1 for a.");
+//   console.log("Press 2 for b.");
+//   console.log("Press 3 for c.");
+//   console.log("Press 4 for d.");
+//   console.log("Press 0 for exit.");
+//   if (num === 1) console.log("a");
+//   else if (num === 2) console.log("b");
+//   else if (num === 3) console.log("c");
+//   else if (num === 4) console.log("1");
+//   else if(num === 0) break;
+// }
