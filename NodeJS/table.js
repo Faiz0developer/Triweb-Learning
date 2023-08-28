f17c5c7ce880f5353f73db4508aac98b4e2f59c1
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-28 23:16:30 +0530

    print a table of a number using loop

---
+++ NodeJS/table.js
@@ -0,0 +1,17 @@
+let start = 1;
+
+let i = start;
+
+let end = 10;
+
+// while (i <= end) {
+//   console.log(i);
+//   i++
+// }
+
+let num = 19;
+
+while (i <= end) {
+  console.log(`${num} * ${i} = ${i * num}`);
+  i++;
+}
