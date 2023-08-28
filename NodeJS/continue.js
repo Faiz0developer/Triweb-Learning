d838f29443fa14f03caa11114f9952073c9ac6ac
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-28 19:16:32 +0530

    practicle implementation of continue with loop

---
+++ NodeJS/continue.js
@@ -0,0 +1,21 @@
+// continue with single loop
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  console.log(i);
+}
+
+console.log("***************************************************");
+
+// continue in nested loop
+
+// continue in inner loop
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 5; j++) {
+    if (j === 3) {
+      continue;
+    }
+    console.log(i, j);
+  }
+}
