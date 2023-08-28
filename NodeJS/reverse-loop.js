de98e70e3510d4bb6954a597049b871de646a57b
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-28 23:21:38 +0530

    print a table of a number using loop in reverse direction

---
+++ NodeJS/reverse-loop.js
@@ -0,0 +1,14 @@
+let start = 10;
+let end = 1;
+let num = 2;
+let i = start;
+
+// while (i > end) {
+//   console.log(i);
+//   i--;
+// }
+
+while (i >= end) {
+  console.log(`${num} * ${i} = ${num * i}`);
+  i--;
+}
