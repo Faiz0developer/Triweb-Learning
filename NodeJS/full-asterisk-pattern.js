d3553cd84492a3af37734ecf3acef327c508c44b
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-29 08:45:14 +0530

    print full asterisk pattern

---
+++ NodeJS/full-asterisk-pattern.js
@@ -0,0 +1,19 @@
+const printAsterisk = (howMAnyTimes) => {
+  let j = 1;
+  while (j <= howMAnyTimes) {
+    console.log("*");
+    j++;
+  }
+};
+
+for (i = 1; i <= 5; i++) {
+  printAsterisk(i);
+  console.log("\n");
+}
+
+i -= 1;
+while (i >= 1) {
+  printAsterisk(i);
+  console.log("\n");
+  i -= 1;
+}
