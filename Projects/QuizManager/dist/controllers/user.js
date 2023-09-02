cfcf55c4b322ecaf8a68b5bd6df711f2267a3601
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-09-02 18:37:27 +0530

    create api for get and update user data from database

--- Projects/QuizManager/dist/controllers/user.js
+++ Projects/QuizManager/dist/controllers/user.js
@@ -12,9 +12,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerUser = void 0;
+exports.updateUser = exports.getUser = exports.registerUser = void 0;
 const user_1 = __importDefault(require("../models/user"));
-const registerUser = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+const registerUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let resp;
     try {
         const user = new user_1.default(req.body);
@@ -38,3 +38,46 @@ const registerUser = (req, res, next) => __awaiter(void 0, void 0, void 0, funct
     }
 });
 exports.registerUser = registerUser;
+// get-->> get user data
+const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // console.log("query", req.query);
+    // console.log("params", req.params);
+    // console.log("params", req.params.userId);
+    let resp;
+    try {
+        const userId = req.params.userId;
+        const user = yield user_1.default.findById(userId, { name: 1, email: 1 });
+        if (!user) {
+            resp = { status: "error", message: "User not found", data: {} };
+            res.send(resp);
+        }
+        else {
+            resp = { status: "success", message: "User found", data: user };
+            res.send(resp);
+        }
+    }
+    catch (err) {
+        resp = { status: "error", message: "Something went wrong", data: {} };
+        res.status(500).send(resp);
+    }
+});
+exports.getUser = getUser;
+//put-->> update user data
+const updateUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let resp;
+    try {
+        const userId = req.body._id;
+        const user = yield user_1.default.findById(userId);
+        if (user) {
+            user.name = req.body.name;
+            yield user.save();
+            resp = { status: "success", message: "User Updated", data: {} };
+            res.send(resp);
+        }
+    }
+    catch (err) {
+        resp = { status: "error", message: "Something went wrong", data: {} };
+        res.status(500).send(resp);
+    }
+});
+exports.updateUser = updateUser;
--- Projects/QuizManager/src/controllers/user.ts
+++ Projects/QuizManager/src/controllers/user.ts
@@ -7,11 +7,7 @@ interface ReturnResponse {
   data: {};
 }
 
-const registerUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const registerUser = async (req: Request, res: Response) => {
   let resp: ReturnResponse;
   try {
     const user = new User(req.body);
@@ -33,4 +29,44 @@ const registerUser = async (
   }
 };
 
-export { registerUser };
+// get-->> get user data
+const getUser = async (req: Request, res: Response) => {
+  // console.log("query", req.query);
+  // console.log("params", req.params);
+  // console.log("params", req.params.userId);
+  let resp: ReturnResponse;
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId, { name: 1, email: 1 });
+    if (!user) {
+      resp = { status: "error", message: "User not found", data: {} };
+      res.send(resp);
+    } else {
+      resp = { status: "success", message: "User found", data: user };
+      res.send(resp);
+    }
+  } catch (err) {
+    resp = { status: "error", message: "Something went wrong", data: {} };
+    res.status(500).send(resp);
+  }
+};
+
+//put-->> update user data
+const updateUser = async (req: Request, res: Response) => {
+  let resp: ReturnResponse;
+  try {
+    const userId = req.body._id;
+    const user = await User.findById(userId);
+    if (user) {
+      user.name = req.body.name;
+      await user.save();
+      resp = { status: "success", message: "User Updated", data: {} };
+      res.send(resp);
+    }
+  } catch (err) {
+    resp = { status: "error", message: "Something went wrong", data: {} };
+    res.status(500).send(resp);
+  }
+};
+
+export { registerUser, getUser, updateUser };
