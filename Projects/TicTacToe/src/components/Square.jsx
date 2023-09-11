26579bc0ab6361b4ab2428f4895eaf034834d5f5
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-09-11 14:54:01 +0530

    using props and usestate hook created 9 squares and can click over it show X

---
+++ Projects/TicTacToe/src/components/Square.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import Styles from "./Square.module.css"
+
+const Square = ({value,onSquareClick}) => {
+
+  return (
+    <button  className={Styles.square} onClick={onSquareClick}>{value}</button>
+  )
+}
+
+export default Square
