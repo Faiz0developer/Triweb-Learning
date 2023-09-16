3706d15df0f1db5457875f9591489020616daad6
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-09-16 15:37:52 +0530

    Created header of my-portfolio website

--- Projects/portfolio/src/components/Header.jsx
+++ Projects/portfolio/src/components/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "../styles/Header.css";
+import MyImage from '../assets/my-img.png'
 
 const Header = () => {
   return (
-    <div></div>
-  )
-}
+    <header className="header-style">
+      <div className="pb-20 pt-10 px-6">
+        <div>
+          <div className="left-container">
+            <div className="img-container">
+              <img
+                src={MyImage}
+                alt="This is my pic"
+                className="w-full h-full object-cover	rounded-full"
+              />
+            </div>
+          </div>
+          <div className="ml-4 mt-8">
+            <ul className="flex flex-col gap-6 text-[#fdfeff] font-light">
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>RESUME</li>
+              <li>SKILLS</li>
+              <li>PORTFOLIO</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
