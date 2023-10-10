bbf4f5aac57d3bdfa3409f414bba1d21ce39d8fd
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-10-10 16:33:53 +0530

    Practice - Practice and implement shallow copy using Object.assign in Array

---
+++ NodeJS/Practice/deepCloning-vs-shallowCopy/shallowCopy/Array-ObjectAsign.js
@@ -0,0 +1,18 @@
+const arr = [1,2,3]
+
+const newArr = [];
+
+Object.assign(newArr,arr)
+
+console.log('Before - arr', arr)         //Before - arr [ 1, 2, 3 ]
+console.log('Before - copyArr', newArr)  // Before - copyArr [ 1, 2, 3 ]
+
+
+
+
+
+arr[0]=8
+
+console.log('After - arr', arr)         //After - arr [ 8, 2, 3 ]
+console.log('After - copyArr', newArr)  //After - copyArr [ 1, 2, 3 ]
+
