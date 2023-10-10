6ba10b56e6c65487527499e42b089548feb291fd
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-10-10 17:24:22 +0530

    Practice - Practice and implement deep copy using lodash package in object

---
+++ NodeJS/Practice/deepCloning-vs-shallowCopy/deepCopy/object-lodash.js
@@ -0,0 +1,17 @@
+const _ = require('lodash')
+
+const obj = {
+    a: { p: 1, q: 2, r: 3 },
+    b: [4, 5, 6],
+    c: [7, 8, 9],
+  };
+  
+  const newObj = _.cloneDeep(obj);
+  
+  console.log("Before - obj", obj); 
+  console.log("Before - newObj", newObj); 
+  
+  obj['a']['p'] = 8;
+  
+  console.log('After - obj', obj)         
+  console.log("After - newObj", newObj); 
